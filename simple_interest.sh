1c06527da3965c9716311baff97ed66df988e51d
Ansh Rawat <83866039+20Ansh02@users.noreply.github.com>
2023-09-27 00:36:48 +0530

    Update simple_interest.sh

--- simple_interest.sh
+++ simple_interest.sh
@@ -1,1 +1,36 @@
+To write a basic script file, you will need to create a new plain text file and save it with a .sh extension. The first line of the script should be the shebang, which tells the operating system which interpreter to use to execute the script. For most bash scripts, the shebang will be #!/bin/bash.
 
+Once you have created the script file and added the shebang, you can start adding commands to the script. Commands are simply instructions that the computer will execute when the script is run.
+
+For example, the following script will print "Hello, world!" to the console:
+
+#!/bin/bash
+
+echo "Hello, world!"
+To run the script, you can open a terminal window and navigate to the directory where the script is located. Then, you can type the following command:
+
+bash script_name.sh
+This will tell the operating system to execute the script using the bash interpreter.
+
+Here is another example of a basic script file:
+
+#!/bin/bash
+
+# This script will create a new directory called "my_dir" in the current directory.
+
+mkdir my_dir
+To run this script, you would type the following command:
+
+bash create_dir.sh
+This would create a new directory called my_dir in the current directory.
+
+You can use script files to automate all sorts of tasks, such as creating and deleting files, running commands, and processing data. As you become more familiar with scripting, you can learn to write more complex scripts that can perform a wide variety of tasks.
+
+Here are some additional tips for writing script files:
+
+Use comments to explain what your script is doing and how it works. Comments are ignored by the interpreter, but they can be very helpful for other people who read your code.
+Use variables to store data that your script will use. Variables can make your code more reusable and easier to maintain.
+Use conditional statements to control the flow of execution in your script. Conditional statements allow you to make decisions about what to do next, based on the values of variables or other conditions.
+Use loops to repeat tasks until a certain condition is met. Loops can be very useful for automating tasks that need to be performed multiple times.
+Test your scripts thoroughly before using them in production. This will help you to identify and fix any errors in your code.
+There are many resources available to help you learn more about scripting. You can find tutorials, documentation, and examples online and in books.
